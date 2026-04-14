2e82b6750a820722a1cdc39f6caaa8ba34e84aee
PeeKDev12 <202903588+PeeKDev12@users.noreply.github.com.>
2026-04-14 14:49:37 +0700

    Fix image pathing issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this line! Replace 'spa-website' with your EXACT GitHub repository name
+  base: '/VibeMerchantWebsite/', 
 })
